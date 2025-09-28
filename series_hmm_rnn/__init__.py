76d58370caa3654b224967f4b4b0dc732e8b5e9a
GabrielNixon <gr2513@nyu.edu>
2025-09-27 20:09:10 -0400

    Create __init__.py

---
+++ series_hmm_rnn/__init__.py
@@ -0,0 +1,9 @@
+from .agents import MFReward, MFChoice, BiasAgent, MBReward
+from .utils import build_Q_seq, make_inputs, nll_loss
+from .models import TinyMoARNN, DiscreteHMM, SeriesHMMTinyMoARNN
+from .data import two_step_mb_generator
+from .train import (
+    train_epoch_tiny, eval_epoch_tiny,
+    train_epoch_series, eval_epoch_series,
+)
+from .metrics import phase_accuracy_permuted, align_gamma
